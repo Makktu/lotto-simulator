3a2a9075ac09d62d9868d03315aa4c62d4282714
Makktu <johnmcna@gmail.com>
2022-02-08 20:42:16 +0000

    working on the logic

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
 "use strict";
 
 let randomNumber;
-let numbersMatching = 0;
-let numbersPicked = [];
-let myNumbers = [7, 9, 12, 14, 15, 36];
+let alreadyPicked = false;
+let bonusBall;
+let numbersMatching = 0; // this variable keeps track of how many 'myNumbers' match the simulated lotto draw numbers
+let numbersPicked = []; // empty array to be filled with a simulated draw of 7 numbers (6x main numbers plus bonus ball)
+let myNumbers = [7, 9, 12, 14, 15, 36]; // the starting myNumbers; the user will be able to change these
 
-const lottoDraw = function () {
-    for (let j = 1; j < 7; j++) {
+const sortNumbers = (arr) => {
+    // sorts an array of numbers into numerical order
+    arr.sort(function (a, b) {
+        return a - b;
+    });
+};
+
+const lottoDraw = () => {
+    numbersPicked = [];
+    for (let j = 1; j < 8; j++) {
         randomNumber = Math.random() * 50 + 1;
         randomNumber = Math.floor(randomNumber);
-        numbersPicked.push(randomNumber);
+        for (let g = 0; g < numbersPicked.length; g++) {
+            if (numbersPicked[g] == randomNumber) {
+                alreadyPicked = true;
+                break;
+            }
+        }
+        if (!alreadyPicked) {
+            numbersPicked.push(randomNumber);
+        } else {
+            console.log("dupe");
+            alreadyPicked = false;
+        }
     }
-    return numbersPicked;
+    bonusBall = numbersPicked[6]; // assign last number picked to be the bonus ball
+    numbersPicked.pop(); // remove last number picked – bonus ball – from the 7 numbers
+    return numbersPicked, bonusBall;
 };
 
 const checkNumbers = (numbersPicked, myNumbers) => {
@@ -26,7 +49,22 @@ const checkNumbers = (numbersPicked, myNumbers) => {
     return numbersMatching;
 };
 
-for (let s = 0; s < 50; s++) {
-    lottoDraw();
-    console.log(checkNumbers(numbersPicked, myNumbers));
-}
+const init = () => {
+    // for (let s = 0; s < 50; s++) {
+    //     lottoDraw();
+    //     console.log(checkNumbers(numbersPicked, myNumbers));
+    // }
+    for (let p = 0; p < 10; p++) {
+        numbersMatching = 0;
+        lottoDraw();
+        console.log(numbersPicked, bonusBall);
+        checkNumbers(numbersPicked, myNumbers);
+        console.log(`You matched ${numbersMatching} for this draw`);
+    }
+};
+
+init();
+
+// 1) check that 'myNumbers' exists
+// 2) make a Lotto draw; sort into numerical order
+// 3) check myNumbers against Lotto draw - counting matches (numerical) – and creating display; sort into numerical order
