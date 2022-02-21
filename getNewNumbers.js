edf230405812573aef04252032865e1632aa1c07
Makktu <johnmcna@gmail.com>
2022-02-21 18:43:32 +0000

    continuing

---
+++ getNewNumbers.js
@@ -0,0 +1,28 @@
+import { sortNumbers } from "./sortNumbers";
+
+export function getNewNumbers() {
+    alert(
+        "You will be asked for 6 numbers, one after the other. Enter them ONE at a time. Press OK to start."
+    );
+    let thisNum;
+    let changedNums = [];
+    for (let d = 0; d < 6; d++) {
+        thisNum = prompt(`Enter number ${d + 1}`);
+        if (thisNum < 1 || thisNum > 50) {
+            alert(
+                "Illegal entry. Numbers must be between 1 and 50 inclusive. Your numbers have not been changed."
+            );
+            return;
+        }
+        changedNums.push(thisNum);
+    }
+    sortNumbers(changedNums);
+    let confirm = prompt(
+        `Your numbers have not yet been changed. Press OK to confirm that you wish to change them to ${changedNums}; CANCEL will cancel this change`
+    );
+    if (confirm == "y" || confirm == "Y") {
+        myNumbers = changedNums;
+        alert(`Your numbers have been changed to ${myNumbers}`);
+        return;
+    }
+}
--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 import { sortNumbers } from "./sortNumbers.js";
+import { getNewNumbers } from "./getNewNumbers.js";
 
 const drawsArea = document.querySelector(".draws");
 
@@ -105,37 +106,5 @@ function alertUser(w, numbersMatching, myNumbers) {
 
     if (bonusMatched) bonusMatched = false;
 }
-
-function getNewNumbers() {
-    alert(
-        "You will be asked for 6 numbers, one after the other. Enter them ONE at a time. Press OK to start."
-    );
-    let thisNum;
-    let changedNums = [];
-    for (let d = 0; d < 6; d++) {
-        thisNum = prompt(`Enter number ${d + 1}`);
-        if (thisNum < 1 || thisNum > 50) {
-            alert(
-                "Illegal entry. Numbers must be between 1 and 50 inclusive. Your numbers have not been changed."
-            );
-            return;
-        }
-        changedNums.push(thisNum);
-    }
-    sortNumbers(changedNums);
-    let confirm = prompt(
-        `Your numbers have not yet been changed. Press OK to confirm that you wish to change them to ${changedNums}; CANCEL will cancel this change`
-    );
-    if (confirm == "y" || confirm == "Y") {
-        myNumbers = changedNums;
-        alert(`Your numbers have been changed to ${myNumbers}`);
-        return;
-    }
-}
-
-// console.log(
-//     "%cHello World",
-//     "font-weight: bold; font-size: 2rem; color: blue;"
-// );
 
 init();
